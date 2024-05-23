2aeba757633c259d723ed0cc7fe4c8620420b611
Rodolpho Reis <rodolphoreis@live.com>
2024-05-23 21:06:58 +0100

    feat: add ResponsiveAppBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import ResponsiveAppBar from "./components/NavBar/ResponsiveAppBar";
+
 function App() {
   return (
     <Container maxWidth="lg">
       <Box sx={{ bgcolor: "#f7f7f7", height: "100vh" }}>
+        <nav>
+          <ResponsiveAppBar />
+        </nav>
       </Box>
     </Container>
   );
