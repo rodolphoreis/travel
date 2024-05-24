30124c71be5de88055dd90f8aa0b696f723d6013
Rodolpho Reis <rodolphoreis@live.com>
2024-05-24 19:38:40 +0100

    style: refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
+
+import { MdExplore } from "react-icons/md";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import ResponsiveAppBar from "./components/NavBar/ResponsiveAppBar";
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      <Box sx={{ bgcolor: "#f7f7f7", height: "100vh" }}>
+    <Box sx={{ bgcolor: "#ffffff", height: "100vh" }}>
+      <header>
+        <img src="./img/london.jpeg" alt="Imagem de Londres" />
         <nav>
           <ResponsiveAppBar />
         </nav>
-      </Box>
-    </Container>
+        <div className="header">
+          <div className="header-texto">
+            <h1>Discover the world with Travelfy!</h1>
+            <p>
+              For a trip to London to be perfect, you need to plan it carefully.
+              A good way to start is to read some interesting details about the
+              city's history, consult answers to frequently asked questions,
+              learn about the main tourist attractions and discover its museums
+              and galleries.
+            </p>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained" sx={{ bgcolor: "rgb(255, 122, 5)" }}>
+                <MdExplore style={{ marginRight: 9 }} />
+                Explorar Destinos
+              </Button>
+            </Stack>
+          </div>
+        </div>
+      </header>
+    </Box>
   );
 }
 
