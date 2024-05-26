43664d98cc49963bf6992cda983cace854860fa4
Rodolpho Reis <rodolphoreis@live.com>
2024-05-26 14:22:20 +0100

    chore: refactor component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
+
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
@@ -11,16 +10,12 @@ interface SidebarProps {
     title: string;
   }>;
   description: string;
-  social: ReadonlyArray<{
-    icon: React.ElementType;
-    name: string;
-    url: string;
-  }>;
+
   title: string;
 }
 
 export default function Sidebar(props: SidebarProps) {
-  const { archives, description, social, title } = props;
+  const { archives, description, title } = props;
 
   return (
     <Grid item xs={12} md={4}>
@@ -42,24 +37,6 @@ export default function Sidebar(props: SidebarProps) {
           key={archive.title}
         >
           {archive.title}
-        </Link>
-      ))}
-      <Typography variant="h6" gutterBottom sx={{ mt: 3 }}>
-        Social
-      </Typography>
-      {social.map((network) => (
-        <Link
-          display="block"
-          variant="body1"
-          target="_blank"
-          href={network.url}
-          key={network.name}
-          sx={{ mb: 0.5 }}
-        >
-          <Stack direction="row" spacing={1} alignItems="center">
-            <network.icon />
-            <span>{network.name}</span>
-          </Stack>
         </Link>
       ))}
     </Grid>
