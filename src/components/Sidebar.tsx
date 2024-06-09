fff5d9390b1fc8c7cb894b5117158664d49a0107
Rodolpho Reis <rodolphoreis@live.com>
2024-06-09 13:17:43 +0100

    style: add margin top

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,7 +19,10 @@ export default function Sidebar(props: SidebarProps) {
 
   return (
     <Grid item xs={12} md={4}>
-      <Paper elevation={0} sx={{ p: 2, bgcolor: "grey.200" }}>
+      <Paper
+        elevation={0}
+        sx={{ p: 2, marginTop: "27px", bgcolor: "grey.200" }}
+      >
         <Typography variant="h6" gutterBottom>
           {title}
         </Typography>
